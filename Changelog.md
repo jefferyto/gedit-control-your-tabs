afadce96a48dd0768702e22a08a623eb391c3fd7
Jeffery To <jeffery.to@gmail.com>
2025-01-01 16:53:48 +0800

    Squashed 'controlyourtabs/utils/' changes from 0c0bd37829d0..97d2f7d5fed2
    97d2f7d5fed2 0.3.0
    b2e9ac827264 change license to GPL-2.0-or-later
    655ed353af28 update changelog format
    22db02e67332 Merge branch 'develop'
    99091c854343 open dev version
    91f1862c7a00 Merge branch 'develop'
    f72f9076acfb rename master to main
    
    git-subtree-dir: controlyourtabs/utils
    git-subtree-split: 97d2f7d5fed2f7f1115a54657a2cf6d980ee0f5f

--- Changelog.md
+++ Changelog.md
@@ -1,8 +1,15 @@
 # Changelog
 
-## 0.2.0 (2017-10-13)
+## [0.3.0] - 2024-12-29
+* Changed license to GPL-2.0-or-later
+
+## [0.2.0] - 2017-10-13
 * Added create_bindings / release_bindings
 * Added to_name, debug_str
 
-## 0.1.0 (2013-10-31)
+## 0.1.0 - 2013-10-31
 * Initial release
+
+
+[0.3.0]: https://github.com/jefferyto/python-gtk-utils/compare/0.2.0...0.3.0
+[0.2.0]: https://github.com/jefferyto/python-gtk-utils/compare/0.1.0...0.2.0
--- __init__.py
+++ __init__.py
@@ -6,9 +6,9 @@
 # Copyright (C) 2013, 2017 Jeffery To <jeffery.to@gmail.com>
 # https://github.com/jefferyto/python-gtk-utils
 #
-# This program is free software: you can redistribute it and/or modify
+# This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
+# the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
@@ -16,8 +16,8 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, see <https://www.gnu.org/licenses/>.
 
 from gi.repository import GObject
 
